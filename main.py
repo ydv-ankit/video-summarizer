2e03319602d2e10ef8adfe996108c560c2e131a4
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-14 23:08:22 +0530

    video upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile
+import math
+import random
+import os
+import video_processing
+
+# FastAPI instance
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"msg":"success"}
+
+@app.post("/upload")
+async def create_upload_file(file: UploadFile):
+    # create a random filename
+    filename: str = str(math.floor(random.random() * 10e10)) + "." + file.filename.split(".")[-1]
+    # check and create "tmp" directory
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
+    # read the file contents and write to a new file inside "tmp" directory
+    contents = await file.read()
+    with open(f"tmp/{filename}", "wb") as f:
+        f.write(contents)
+    
+    # extract audio from video
+    video_processing.process_video(f"tmp/{filename}")
+    return {"msg": "video processed", "data": None}

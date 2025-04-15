ad5419bc0b389e66d26ed8a48cce64ded5cff81d
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-15 10:03:36 +0530

    google gemini

--- video_processing.py
+++ video_processing.py
@@ -1,73 +1,73 @@
 from moviepy import VideoFileClip
 import os 
-from pydub import AudioSegment
-from openai import OpenAI
-from dotenv import load_dotenv 
-from pydantic import BaseModel
+import json
+from google import genai
+from google.genai import types
+import env
 
-load_dotenv(dotenv_path=".env")
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=OPENAI_API_KEY)
+gemini = genai.Client(api_key=env.GEMINI_API_KEY)
 
 def process_video(video_path):
     """
     Process the video file
     """
-    filename, _ = os.path.splitext(video_path)
-    clip = VideoFileClip(video_path)
-    audioFilePath = f"{filename}.wav"
-    clip.audio.write_audiofile(audioFilePath)
-    transcription = transcribe_audio(audioFilePath)
-    summary = summarizer(transcription)
-    return summary
+    try:
+        filename, _ = os.path.splitext(video_path)
+        # extract audio
+        clip = VideoFileClip(video_path)
+        audioFilePath = f"{filename}.wav"
+        clip.audio.write_audiofile(audioFilePath)
+        # get transcription from audio
+        transcription = transcribe(audio_path=audioFilePath)
+        # summarize transcripted text
+        summary = summarizer(transcription)
+        # cleanup files
+        cleanup([video_path, audioFilePath])
+        return summary
+    except Exception as e:
+        print(e)
+        return None
 
-def convert_to_mono_16k(audio_file_path, output_dir="tmp"):
-    """Converts audio to mono and 16kHz, returns the path to the converted audio."""
-    sound = AudioSegment.from_file(audio_file_path)
-    sound = sound.set_channels(1)  # Mono
-    sound = sound.set_frame_rate(16000)  # 16kHz
+def transcribe(audio_path):
+    audio_file = gemini.files.upload(file=audio_path)
+    prompt = 'Generate a transcript of the speech.'
 
-    # Generate a unique filename for the mono version
-    converted_file_name = f"{audio_file_path.split('/')[-1]}"
-    converted_file_path = os.path.join(output_dir, converted_file_name)
-
-    # Export the converted audio file
-    sound.export(converted_file_path, format="wav")
-    return converted_file_path
-
-def transcribe_audio(audio_path):
-    # convert to mono and 16kHz audio
-    mono_audio_path = convert_to_mono_16k(audio_path)
-    print(mono_audio_path)
-
-    with open(mono_audio_path, "rb") as audio_file:
-        transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
-
-    print(transcript)
-    return transcript.text
-
-# to get formatted response from LLM
-class SummarizerFormat(BaseModel):
-    summary: str
+    response = gemini.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=[prompt, audio_file]
+    )
+    return response.text
 
 def summarizer(text: str):
-    data = client.beta.chat.completions.parse(
-        messages=[
-               {
-                      "role": "system",
-                      "content": """
-                        You are given a transcription of a video file.
-                        Your task is to summarize given transcription and provide a summary containing important points.
-                        Return the summary content into markdown compatible syntax.
+    try:
+        response = gemini.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction="""
+                You are given a transcription of a audio file.
+                Your task is to summarize given transcription and provide a summary containing important points.
+                Return the summary content into markdown compatible syntax that includes:
+                    - summary of the content
+                    - important points (if any)
+                
+                Don't give intermediate steps or your descibing, give me only required content.
+                ---
+                Use this JSON schema:
 
-					return response in json format.
-                        """,
-               },
-               { "role": "user", "content": f"{text}" },
-        ],
-        model="gpt-4o-mini",
-        response_format=SummarizerFormat
-	);
+                Summary= {'summary': str, 'important_points': List[str]}
+                Return: list[Summary]
+            """),
+        contents=text
+        )
+        clean_json_str = response.text.strip('```json')
 
-    return data.choices[0].message.content
+        data = json.loads(clean_json_str)
+        print(data)
+        return data[0]
+    except Exception as e:
+        print(e)
+        return None
+    
+def cleanup(files: list[str]):
+    for path in files:
+        os.unlink(path)

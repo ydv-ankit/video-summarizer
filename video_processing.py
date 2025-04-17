2405ad03f40f2bffa90f5e643d894fab2c1b7b31
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-17 19:19:03 +0530

    fix: gemini response

--- video_processing.py
+++ video_processing.py
@@ -1,9 +1,9 @@
 from moviepy import VideoFileClip
 import os 
-import json
 from google import genai
 from google.genai import types
 import env
+from pydub import AudioSegment
 
 gemini = genai.Client(api_key=env.GEMINI_API_KEY)
 
@@ -20,16 +20,31 @@ def process_video(video_path):
         # get transcription from audio
         transcription = transcribe(audio_path=audioFilePath)
         # summarize transcripted text
+        print(transcription)
         summary = summarizer(transcription)
         # cleanup files
-        cleanup([video_path, audioFilePath])
         return summary
     except Exception as e:
         print(e)
         return None
+    
+def convert_to_mono_16k(audio_file_path, output_dir="tmp"):
+    """Converts audio to mono and 16kHz, returns the path to the converted audio."""
+    sound = AudioSegment.from_file(audio_file_path)
+    os.unlink(audio_file_path)
+    sound = sound.set_channels(1)  # Mono
+    sound = sound.set_frame_rate(16000)  # 16kHz
+
+    converted_file_name = f"{audio_file_path.split('/')[-1]}"
+    converted_file_path = os.path.join(output_dir, converted_file_name)
+
+    # Export the converted audio file
+    sound.export(converted_file_path, format="wav")
+    return converted_file_path
 
 def transcribe(audio_path):
-    audio_file = gemini.files.upload(file=audio_path)
+    mono_audio = convert_to_mono_16k(audio_path)
+    audio_file = gemini.files.upload(file=mono_audio)
     prompt = 'Generate a transcript of the speech.'
 
     response = gemini.models.generate_content(
@@ -49,25 +64,20 @@ def summarizer(text: str):
                 Return the summary content into markdown compatible syntax that includes:
                     - summary of the content
                     - important points (if any)
+                    - any other relevant information (if any)
                 
                 Don't give intermediate steps or your descibing, give me only required content.
-                ---
-                Use this JSON schema:
-
-                Summary= {'summary': str, 'important_points': List[str]}
-                Return: list[Summary]
             """),
         contents=text
         )
-        clean_json_str = response.text.strip('```json')
+        print(response.text)
+        # clean the response to get only the json part
+        # clean_json_str = response.text.strip('```json')
 
-        data = json.loads(clean_json_str)
-        print(data)
-        return data[0]
+        # data = json.loads(clean_json_str)
+        # print(data)
+        # return data[0]
+        return response.text
     except Exception as e:
         print(e)
         return None
-    
-def cleanup(files: list[str]):
-    for path in files:
-        os.unlink(path)
